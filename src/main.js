8e5558432a01f2b7056ae6dae259b96c51076545
pengshaomei <18358352865@163.com>
2018-11-02 08:51:41 +0800

    登录，首页，用户完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import HttpTool from '@/plugins/http.js'
 import ElementUI from 'element-ui'
-// import '../assets/css/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/index.css'
+
+import router from './router'
+
+Vue.use(HttpTool)
+Vue.use(ElementUI)
+
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
+
+import Login from '@/views/login.vue'
+import Home from '@/views/home.vue'
+import Users from '@/views/users.vue'
+
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+
+    routes: [{
+        name: 'home',
+        path: '/',
+        component: Home,
+        children: [{ path: '/users', component: Users }]
+    }, {
+        name: 'login',
+        path: '/login',
+        component: Login
+    }]
 })
